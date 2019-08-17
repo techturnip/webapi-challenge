fe1c81be5d4ec042be8a3d03f14ddd98058e1731
Tyler Turnipseed <tyler@techturnip.us>
2019-08-17 12:40:48 -0400

    added endpoints to get projects and get project by id

---
+++ middleware/routerMiddleware.js
@@ -0,0 +1,57 @@
+module.exports = {
+  validateProjectId,
+  validateProject
+}
+
+// Import project model ---------------------------|
+const Projects = require('../projects/projectModel.js')
+// ------------------------------------------------|
+// CUSTOM PROJECTS MIDDLEWARE =====================|
+// ================================================|
+function validateProjectId(req, res, next) {
+  const { id } = req.params
+
+  Projects.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project
+        next()
+      } else {
+        res.status(404).json({ message: 'Invalid project id' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'Error processing request'
+      })
+    })
+}
+// ------------------------------------------------|
+function validateProject(req, res, next) {
+  if (req.body && Object.keys(req.body).length > 0) {
+    const { name, description } = req.body
+
+    if (name && description) {
+      if (
+        name.length > 2 &&
+        typeof name === 'string' &&
+        description.length > 2 &&
+        typeof description === 'string'
+      ) {
+        next()
+      } else {
+        res.status(400).json({
+          message:
+            'Name and description fields must be strings and cannot be empty'
+        })
+      }
+    } else {
+      res
+        .status(400)
+        .json({ message: 'Missing required name and/or description field' })
+    }
+  } else {
+    res.status(400).json({ message: 'Missing project data' })
+  }
+}
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -3,11 +3,56 @@
 // import express router --------------------------|
 const router = require('express').Router()
 // bring in DB operations -------------------------|
-const Project = require('./projectModel.js')
+const Projects = require('./projectModel.js')
+// bring in custom middleware ---------------------|
+const {
+  validateProjectId,
+  validateProject
+} = require('../middleware/routerMiddleware.js')
 // ------------------------------------------------|
 // REQ HANDLERS ===================================|
 // ================================================|
 // Base URL '/api/projects/'
+// ------------------------------------------------|
+// GET Request returns an array of projects -------|
+router.get('/', async (req, res) => {
+  try {
+    const projects = await Projects.get()
+
+    res.status(200).json(projects)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Could not retrieve projects from database'
+    })
+  }
+})
+// ------------------------------------------------|
+// GET Request returns a project by id ------------|
+router.get('/:id', validateProjectId, async (req, res) => {
+  try {
+    res.status(200).json(req.project)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Could not retrieve projects from database'
+    })
+  }
+})
+// ------------------------------------------------|
+// POST Request creates a new project in db -------|
+router.post('/', validateProject, async (req, res) => {
+  try {
+    const newProject = await Projects.insert(req.body)
+
+    res.status(200).json(newProject)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Could not create new project in database'
+    })
+  }
+})
 
 // ------------------------------------------------|
 // CUSTOM MIDDLEWARE ==============================|
